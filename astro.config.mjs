d8c5b63920d0907732f59e23f532822e22cc8b62
Pablo Fergus <pablofergus@gmail.com>
2024-09-28 01:23:57 +0200

    Brutal added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
-	//integrations: [
-	//	preact(),
-	//	react(), tailwind()],
+	integrations: [sitemap(), UnoCSS({ injectReset: true })],
 	site:'https://pablofergus.com',
 	output: 'static',
 	outDir: './docs',
