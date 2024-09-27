08ac09116378028c45021744263da5304a067a8e
Pablo Fergus <pablofergus@gmail.com>
2024-09-28 00:21:23 +0200

    Uptated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	//integrations: [
+	//	preact(),
+	//	react(), tailwind()],
+	site:'https://pablofergus.com',
+	output: 'static',
+	outDir: './docs',
+	build: {
+		assets: 'astro'
+	}
+
+});
